f6fee3b53fc9928d8eaab7437748ed18f2eb2ab2
IvanChernukhin <ivan.chernukhin@twisto.cz>
2024-10-14 11:39:18 +0300

    [BF] updated keyboardAsState impl

--- app/src/main/java/com/example/pdp/ui/widgets/Utils.kt
+++ app/src/main/java/com/example/pdp/ui/widgets/Utils.kt
@@ -1,45 +1,38 @@
 package com.example.pdp.ui.widgets
 
-import android.graphics.Rect
 import android.view.ViewTreeObserver
 import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
-
-enum class Keyboard {
-    Opened, Closed
-}
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat.Type.ime
 
 @Composable
-fun keyboardAsState(): State<Keyboard> {
-    val keyboardState = remember { mutableStateOf(Keyboard.Closed) }
+fun keyboardAsState(): State<Boolean> {
     val view = LocalView.current
+    var isImeVisible by remember { mutableStateOf(false) }
 
-    DisposableEffect(view) {
-        val onGlobalListener = ViewTreeObserver.OnGlobalLayoutListener {
-            val rect = Rect()
-            view.getWindowVisibleDisplayFrame(rect)
-            val screenHeight = view.rootView.height
-            val keypadHeight = screenHeight - rect.bottom
-            keyboardState.value = if (keypadHeight > screenHeight * 0.15) {
-                Keyboard.Opened
-            } else {
-                Keyboard.Closed
-            }
+    DisposableEffect(LocalWindowInfo.current) {
+        val listener = ViewTreeObserver.OnPreDrawListener {
+            isImeVisible = ViewCompat.getRootWindowInsets(view)
+                ?.isVisible(ime()) == true
+            true
         }
-        view.viewTreeObserver.addOnGlobalLayoutListener(onGlobalListener)
-
+        view.viewTreeObserver.addOnPreDrawListener(listener)
         onDispose {
-            view.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalListener)
+            view.viewTreeObserver.removeOnPreDrawListener(listener)
         }
     }
-
-    return keyboardState
+    return rememberUpdatedState(isImeVisible)
 }
 
 @Composable
